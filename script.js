3f8cf54e6bcd07927ed529636e58d8f2e8f9a044
CodingWithDude <codingwithdude@gmail.com>
2022-06-29 15:47:09 -0700

    Reworked code from scratch LOL
    Started from scratch as was making too many dom changes just in js
    Changed index.html to a base layout of the elements needed
    changed script.js to create squares based on slider input

--- script.js
+++ script.js
@@ -1,59 +1,25 @@
-// Create body
-const body = document.body;
+function populateBoard(size) {
+  let board = document.querySelector(".board");
+  // Remove old squares
+  let squares = board.querySelectorAll("div");
+  squares.forEach((div) => div.remove());
+  // Update sliderOutput
+  let sliderOutput = document.querySelector(".sliderOutput");
+  sliderOutput.innerHTML = size + " x " + size;
 
-// Create header
-const header = document.createElement("header");
-header.className = "header";
-body.append(header);
+  board.style.gridTemplateColumns = `repeat(${size} , 1fr)`;
+  board.style.gridTemplateRows = `repeat(${size} , 1fr)`;
 
-// Create title
-const title = document.createElement("div");
-title.className = "title";
-title.textContent = "Etch-a-Sketch";
-header.append(title);
+  let amount = size * size;
+  for (let i = 0; i < amount; i++) {
+    let square = document.createElement("div");
+    square.style.backgroundColor = "blue";
+    board.insertAdjacentElement("beforeend", square);
+  }
+}
 
-// Create main
-const main = document.createElement("main");
-main.className = "main";
-body.append(main);
+function changeSize(input) {
+  populateBoard(input);
+}
 
-// Create settings
-const settings = document.createElement("div");
-settings.className = "settings";
-main.append(settings);
-
-// Create slider container
-const sliderContainer = document.createElement("div");
-sliderContainer.className = "sliderContainer";
-settings.append(sliderContainer);
-
-// Create slider value text
-const sliderValue = document.createElement("div");
-sliderValue.className = "sliderValue";
-sliderValue.innerHTML = "16 x 16";
-sliderContainer.append(sliderValue);
-
-// Create slider in settings
-const slider = document.createElement("input");
-slider.className = "slider";
-slider.type = "range";
-slider.value = "16";
-slider.min = "1";
-slider.max = "64";
-sliderContainer.append(slider);
-
-// Function to update slider value text
-let sliderOutput = document.getElementsByClassName("sliderValue");
-slider.oninput = function () {
-  sliderValue.innerHTML = slider.value + " x " + slider.value;
-};
-
-// Create grid
-const grid = document.createElement("div");
-grid.className = "grid";
-main.append(grid);
-
-// Create footer
-const footer = document.createElement("footer");
-footer.className = "footer";
-main.append(footer);
+populateBoard(16);
